ef998a640b43847ce4c1c1d69093e434901e2efc
Mohan Gote <mohan.gote@gmail.com>
2010-03-28 20:57:16 -0400

    new code

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   def registration_confirmation(assessmentform)
     recipients assessmentform.email
-    from       'ATCS Inc. <info@upgradeitskills.com>'
+    from       'uupgradeitskills@gmail.com'
     subject    "Thank you for Registering for Free Assessment Test"
     body       :assessmentform => assessmentform
   end
